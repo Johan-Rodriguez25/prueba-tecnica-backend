0ab377c4161dd9c04bf3170b09dffbba2fdd94d6
Johan Rodriguez <88631504+Johan-Rodriguez25@users.noreply.github.com>
2026-04-30 09:40:23 -0500

    feat(api-gateway): initialize express server with TypeScript and tooling
    Add initial API gateway service with Express.js and TypeScript.
    Includes cluster support, security middleware (helmet, cors), and development tooling (ESLint, Prettier, tsconfig).
    Sets up basic project structure with package.json, gitignore, and entry point.

---
+++ api-gateway/src/modules/main/server.ts
@@ -0,0 +1,82 @@
+import express, { Application } from "express";
+
+import compression from "compression";
+import cors from "cors";
+import dotenvFlow from "dotenv-flow";
+import morgan from "morgan";
+import helmet from "helmet";
+
+dotenvFlow.config({
+  silent: true,
+});
+
+class Server {
+  private port: number;
+  public static instance: Server;
+  public app: Application;
+  private apiPath = {
+    movies: "/v1/api/movies",
+    uploadPoster: "/v1/api/upload/poster",
+  };
+
+  private constructor() {
+    this.app = express();
+    this.port = Number(process.env.PORT) || 3001;
+    this.init();
+  }
+
+  public static getInstance(): Server {
+    if (!Server.instance) {
+      Server.instance = new Server();
+    }
+
+    return Server.instance;
+  }
+
+  private async init(): Promise<void> {
+    try {
+      this.listenStatusConnection();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  private async listenStatusConnection() {
+    try {
+      this.middlewares();
+      this.routes();
+      this.listen();
+    } catch (error) {
+      throw new Error("El servidor no se pudo iniciar");
+    }
+  }
+
+  private middlewares(): void {
+    this.app.use(cors());
+    this.app.use(
+      express.urlencoded({
+        limit: "6mb",
+        extended: true,
+        parameterLimit: 60000,
+      }),
+    );
+    this.app.use(express.json({ limit: "6mb" }));
+    this.app.use(morgan("dev"));
+    this.app.use(helmet());
+    this.app.use(compression({ level: 9 }));
+  }
+
+  private routes(): void {
+    this.app.get("/", (req: any, res: any) =>
+      res.status(200).json({ ok: true }),
+    );
+  }
+
+  private listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Servidor corriendo en ${this.port}`);
+    });
+  }
+}
+
+export default Server;
